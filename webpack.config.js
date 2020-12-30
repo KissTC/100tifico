a51ef6bafb6312249bf0f7f19754e6ea94b483e3
Julio Paredes <ing.julio.code@gmail.com>
2020-12-29 21:33:59 -0600

    Runtas dinamicas y archivos de utilerias agregados

--- webpack.config.js
+++ webpack.config.js
@@ -26,12 +26,12 @@ module.exports = {
         ]
     },
     plugins: [
-        new HtmlWebpackPlugin([
+        new HtmlWebpackPlugin(
             {
                 inject: true,
                 template: './public/index.html',
                 filename: './index.html'
             }
-        ]) //Permite trabajar con los archivos HTML
+        ) //Permite trabajar con los archivos HTML
     ]
 }
